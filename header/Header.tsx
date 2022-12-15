dd3604f78b381ee838698bd21531e3525eea02ab
Zach Miller <home@mac-2.local>
2022-12-15 14:38:57 -0500

    dont know chnages that need to be made for text to move right in more. Curious to see how this works out

--- header/Header.tsx
+++ header/Header.tsx
@@ -1,11 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import classes from './Header.module.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 interface Size {
   width: number | undefined;
   height: number | undefined;
 }
 
+const HeaderMain = styled.header`
+  display: flex;
+  justify-content: center;
+`;
+
+const HeaderLinks = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  align-items: center;
+`;
+
+const HeaderLinkUl = styled.ul`
+  list-style: none;
+  text-align: right;
+`;
+
+const Dropdown = styled.ul`
+  position: absolute;
+  background: #f9fafc;
+  display: none;
+  right: 0;
+  top: 48px;
+  padding: 4px;
+  border: solid 1px #d0d1d9;
+  border-radius: 4px;
+  text-align: right;
+  &:hover {
+    display: block;
+  };
+`;
+
+const HeaderLinkLi = styled.li`
+  padding: 8px;
+  display: block;
+  border: solid 1px transparent;
+  margin: 0 4px;
+  text-align: center;
+  @media (max-width: 700px) {
+    margin: 0 1px;
+    padding: 4px;
+  }
+`;
+
+const HeaderLinkA = styled.a`
+  @media (max-width: 700px) {
+    margin: 0 1px;
+    padding: 4px;
+  }
+  &:hover ~ ${Dropdown} {
+    display: block;
+  }
+
+  &:hover {
+    border-radius: 3px;
+    border: solid 1px #d0d1d9;
+    text-decoration: none;
+  }
+`;
 
 function useWindowSize() {
   const [windowSize, setWindowSize] = useState<Size>({
@@ -20,27 +79,27 @@ function useWindowSize() {
         height: window.innerHeight,
       });
     }
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
   return windowSize;
 }
 
-export function Header(props: { siteName: string}) {
-  const size: Size = useWindowSize()
+export function Header(props: { siteName: string }) {
+  const size: Size = useWindowSize();
 
   const allSites = [
-    { name: 'cryptofees.info', url: 'https://cryptofees.info/' },
-    { name: 'cryptoflows.info', url: 'https://cryptoflows.info/' },
-    { name: 'moneyprinter.info', url: 'https://moneyprinter.info/' },
-    { name: 'l2fees.info', url: 'https://l2fees.info/' },
-    { name: 'simplestakers.info', url: 'https://simplestakers.info/' },
-    { name: 'openorgs.info', url: 'https://openorgs.info/' },
-    { name: 'ethburned.info', url: 'https://ethburned.info/' },
-    { name: 'money-movers.info', url: 'https://money-movers.info/' },
-    { name: 'stakers.info', url: 'https://stakers.info/' },
-    { name: 'ethereumnodes.com', url: 'https://ethereumnodes.com/' },
+    { name: "cryptofees.info", url: "https://cryptofees.info/" },
+    { name: "cryptoflows.info", url: "https://cryptoflows.info/" },
+    { name: "moneyprinter.info", url: "https://moneyprinter.info/" },
+    { name: "l2fees.info", url: "https://l2fees.info/" },
+    { name: "simplestakers.info", url: "https://simplestakers.info/" },
+    { name: "openorgs.info", url: "https://openorgs.info/" },
+    { name: "ethburned.info", url: "https://ethburned.info/" },
+    { name: "money-movers.info", url: "https://money-movers.info/" },
+    { name: "stakers.info", url: "https://stakers.info/" },
+    { name: "ethereumnodes.com", url: "https://ethereumnodes.com/" },
   ];
 
   const currentSite = allSites.find((site) => props.siteName == site.name);
@@ -51,53 +110,52 @@ export function Header(props: { siteName: string}) {
   let mainSites: { name: string; url: string }[] = [];
   let extraSites: { name: string; url: string }[] = [];
 
-  if (size.width >= 330) {
-    mainSites = allSites.slice(0, 1);
-    extraSites = allSites.slice(1);
-  }
+  if (size.width !== undefined) {
+    if (size.width >= 330) {
+      mainSites = allSites.slice(0, 1);
+      extraSites = allSites.slice(1);
+    }
 
-  if (size.width >= 768) {
-    mainSites = allSites.slice(0, 3);
-    extraSites = allSites.slice(3);
-  }
+    if (size.width >= 768) {
+      mainSites = allSites.slice(0, 3);
+      extraSites = allSites.slice(3);
+    }
 
-  if (size.width >= 1366) {
-    mainSites = allSites.slice(0, 4);
-    extraSites = allSites.slice(4);
-  }
+    if (size.width >= 1366) {
+      mainSites = allSites.slice(0, 4);
+      extraSites = allSites.slice(4);
+    }
 
-  if (size.width >= 1440) {
-    mainSites = allSites.slice(0, 6);
-    extraSites = allSites.slice(6);
+    if (size.width >= 1440) {
+      mainSites = allSites.slice(0, 6);
+      extraSites = allSites.slice(6);
+    }
   }
 
   return (
-    <header>
-      <ul className={classes["header-links"]}>
-        <li className={classes["header-link"]}>
-          <div>{currentSite?.name}</div>
-        </li>
+    <HeaderMain>
+      <HeaderLinks>
+        <HeaderLinkLi>{currentSite?.name}</HeaderLinkLi>
         {mainSites?.map((el: { name: string; url: string }, index) => {
           return (
-            <li key={index} className={classes["header-link"]}>
-              <a href={el?.url}>{el?.name}</a>
-            </li>
+            <HeaderLinkLi key={index}>
+              <HeaderLinkA href={el?.url}>{el?.name}</HeaderLinkA>
+            </HeaderLinkLi>
           );
         })}
-        <li className={classes["header-link"]}>
-          <a>More</a>
-
-          <ul className={classes["dropdown"]}>
+        <HeaderLinkA>More</HeaderLinkA>
+        <Dropdown>
+          <HeaderLinkUl>
             {extraSites?.map((el: { name: string; url: string }, index) => {
               return (
-                <li key={index} className={classes["header-link"]}>
-                  <a href={el?.url}>{el?.name}</a>
-                </li>
+                <HeaderLinkLi key={index}>
+                  <HeaderLinkA href={el?.url}>{el?.name}</HeaderLinkA>
+                </HeaderLinkLi>
               );
             })}
-          </ul>
-        </li>
-      </ul>
-    </header>
+          </HeaderLinkUl>
+        </Dropdown>
+      </HeaderLinks>
+    </HeaderMain>
   );
 }
